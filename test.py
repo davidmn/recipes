416c2aa8b30832b4b7370f9e537581dd3231a78b
David Megins-Nicholas <dave@davidmn.org>
2018-05-09 15:59:01 +0100

    the testing flower blooms for issue #3

---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class RecipeTests(unittest.TestCase):
+    def test_dummy(self):
+        self.assert_(True, 'True should be true')
+
+if __name__ == '__main__':
+    unittest.main()
